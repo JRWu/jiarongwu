47b010e0f79a33160e6fa2a1104fa820b1aa6911
JRWu <jwu424@gmail.com>
2024-08-07 00:30:49 -0400

    Reverting to object.

--- pages/resume.js
+++ pages/resume.js
@@ -2,14 +2,14 @@ export default function Resume() {
   // TODO: Populate this w/ Latex Template, Github for Latex and the resume PDF?
   return (
     <div>
-      <iframe
-        src="/static/documents/JiaRongWu_CV_2024.pdf"
+      <object
+        data="/static/documents/JiaRongWu_CV_2024.pdf"
         type="application/pdf"
         width="100%"
         height="600px"
       >
         <p> Your web browser does not have a PDF plugin. </p>
-      </iframe>
+      </object>
     </div>
   )
 }

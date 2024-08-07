cd415587dbfd909e3b04283ba5972dddccc79fd0
JRWu <jwu424@gmail.com>
2024-08-07 00:25:00 -0400

    Revert "Updating to use object instead of embed."
    This reverts commit 0f2635112e4de482f611276b49f3f4e53006bcab.

--- pages/resume.js
+++ pages/resume.js
@@ -2,14 +2,12 @@ export default function Resume() {
   // TODO: Populate this w/ Latex Template, Github for Latex and the resume PDF?
   return (
     <div>
-      <object
-        data="/static/documents/JiaRongWu_CV_2024.pdf"
+      <embed
+        src="/static/documents/JiaRongWu_CV_2024.pdf"
         type="application/pdf"
         width="100%"
-        height="600px"
-      >
-        <p> Your web browser does not have a PDF plugin. </p>
-      </object>
+        height="1000px"
+      />
     </div>
   )
 }
